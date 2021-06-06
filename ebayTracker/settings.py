14955b4618ca426d721aa919ef6b330944293a14
Al Rifat <52749149+AlDevStuff@users.noreply.github.com>
2021-06-05 20:17:12 -0400

    Update settings.py

--- ebayTracker/settings.py
+++ ebayTracker/settings.py
@@ -34,8 +34,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ['EMAIL_USER']
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASSWORD']
+EMAIL_HOST_USER = 'alrifat088@gmail.com'
+EMAIL_HOST_PASSWORD = 'Mariahal1234'
 
 
 # Application definition
