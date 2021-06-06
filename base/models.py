5b5bea1c0e81d536339586bcf2b5544dfc3710d3
Al Rifat <52749149+AlDevStuff@users.noreply.github.com>
2021-06-05 21:09:40 -0400

    Update models.py

--- base/models.py
+++ base/models.py
@@ -36,7 +36,8 @@ class Item(models.Model):
         body = f'''The Ebay product {self.url} has reached your target price of ${self.target_price}. Thus, we will stop notifying you. Thank you for using Ebay Price Alerter!\n\n\n
             Ebay Price Alerter :)
             '''
-        if self.old_price <= self.target_price and self.old_price != 0:
+#         if self.old_price <= self.target_price and self.old_price != 0:
+          if self.current_price <= self.target_price:
             email = EmailMessage('Ebay Price Alerter: Target Price Reached', body, settings.EMAIL_HOST_USER,
                                  to=[self.receiver_email], )
             email.fail_silently = False
